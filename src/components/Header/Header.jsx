0bc988481d52e16629f1c6fb9a2eacaf61662113
eduenez33 <duenezedwin33@gmail.com>
2025-06-25 16:11:06 -0600

    create Header component
    This commit introduces a new, standalone `Header` component that includes
    a logo, the user's name and avatar, and a "Add clothes" button.
    
    The component is designed to be reusable across different pages of the application
    and adheres closely to the provided Figma design specifications.
    
    Key features implemented:
    - **Logo Integration**: Displays the application's brand logo.
    - **User Profile Display**: Shows the logged-in user's name and avatar.
    - **Add Clothes Button**: A button that will open a modal.
    
    This component lays the groundwork for a more consistent and user-friendly navigation experience.
    
    Future improvements:
    - Implement modal opening functionality when the "Add clothes" button is clicked.
    - Dynamically change the current date being displayed
    - Responsive design to accomodate mobile (smaller screens)

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import "./Header.css";
+import logo from "../../assets/wtwr-logo.svg";
+import avatar from "../../assets/avatar.png";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <img src={logo} alt="What to Wear" className="header__logo" />
+        <p className="header__date-location">DATE, LOCATION</p>
+        <button className="header__add-clothes-btn">+ Add clothes</button>
+        <div className="header__user-container">
+          <p className="header__username">Terrence Tegegne</p>
+          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Header;
