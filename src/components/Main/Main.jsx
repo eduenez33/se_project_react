7620112618aea88e4e1b905fc1537c5b7749b281
Edwin Duenez <duenezedwin33@gmail.com>
2025-06-27 10:22:30 -0600

    render default clothing items using ItemCard
    Integrates the `ItemCard` component into the `Main` component,
    displaying a list of `defaultClothingItems`. This provides a visual
    representation of suggested clothing based on the weather, leveraging
    the newly created `ItemCard` for each item.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,8 @@
 import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
+import { defaultClothingItems } from "../../utils/constants";
+
+import "./Main.css";
 
 function Main() {
   return (
@@ -8,6 +12,11 @@ function Main() {
         <p className="cards__text">
           Today is 75&deg; F / You may want to wear:
         </p>
+        <ul className="cards__list">
+          {defaultClothingItems.map((item) => {
+            return <ItemCard key={item._id} item={item} />;
+          })}
+        </ul>
       </section>
     </main>
   );
