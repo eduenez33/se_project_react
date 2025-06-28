d07d8f2fae6ded770d9c8213fefe170843a2b3ed
eduenez33 <duenezedwin33@gmail.com>
2025-06-28 15:28:46 -0600

    change logic for adding style class

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -8,7 +8,7 @@ function ModalWithForm({
   handleModalClose,
 }) {
   return (
-    <div className={`modal ${activeModal ? "modal_opened" : ""}`}>
+    <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button
