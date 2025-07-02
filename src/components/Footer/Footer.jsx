d84ebab38a883533e9da82a89a87003aa0ef0edf
Edwin Duenez <duenezedwin33@gmail.com>
2025-07-02 15:07:08 -0600

    remove unnecessary react fragment, replace with semantic tag

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,10 @@ function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <>
-      <section className="footer">
-        <p className="footer__dev">Developed by Edwin Duenez</p>
-        <p className="footer__year">{currentYear}</p>
-      </section>
-    </>
+    <footer className="footer">
+      <p className="footer__dev">Developed by Edwin Duenez</p>
+      <p className="footer__year">{currentYear}</p>
+    </footer>
   );
 }
 
