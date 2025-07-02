0e56bfe53162527e1bee76365cae4d1f10ac0808
Edwin Duenez <duenezedwin33@gmail.com>
2025-07-02 15:45:52 -0600

    close active modal when escape key is pressed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -33,6 +33,22 @@ function App() {
   };
 
   useEffect(() => {
+    if (!activeModal) return;
+
+    const handleEscClose = (e) => {
+      if (e.key === "Escape") {
+        handleModalClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleEscClose);
+
+    return () => {
+      document.removeEventListener("keydown", handleEscClose);
+    };
+  }, [activeModal]);
+
+  useEffect(() => {
     getWeather(coordinates, APIkey)
       .then((data) => {
         const filteredData = filterWeatherData(data);
