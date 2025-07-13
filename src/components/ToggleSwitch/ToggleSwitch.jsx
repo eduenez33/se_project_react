0a666380ff54dd1d6bf6c9b7c9d8bfc5f7d5f4f1
Edwin Duenez <duenezedwin33@gmail.com>
2025-07-13 12:49:14 -0600

    create ToggleSwitch component for unit selection
    Adds a new reusable `ToggleSwitch` component along with its dedicated styles.
    
    This component provides the user interface for switching between Fahrenheit
    and Celsius temperature units. It is designed to consume the
    `CurrentTemperatureUnitContext` to get and update the active unit.

---
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,27 @@
+import { useContext, useState } from "react";
+
+import "./ToggleSwitch.css";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
+
+function ToggleSwitch() {
+  const { currentTemperatureUnit, handleToggleSwitchChange } = useContext(
+    CurrentTemperatureUnitContext
+  );
+
+  return (
+    <label className="toggleSwitch__label">
+      <input
+        className="toggleSwitch__input"
+        type="checkbox"
+        name="unit_preference"
+        checked={currentTemperatureUnit === "F"}
+        onChange={handleToggleSwitchChange}
+      />
+      <span className="toggleSwitch__text toggleSwitch__text_type_f">F</span>
+      <span className="toggleSwitch__text toggleSwitch__text_type_c">C</span>
+      <span className="toggleSwitch__switch" />
+    </label>
+  );
+}
+
+export default ToggleSwitch;
