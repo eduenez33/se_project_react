c1804e142a14b9bb403331a25bcff3fffd656250
Edwin Duenez <duenezedwin33@gmail.com>
2025-07-15 14:26:16 -0600

    add links that direct to routes
    I wrapped some elements in a Link component that will direct to certain
    defined routes.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import "./Header.css";
 import logo from "../../assets/wtwr-logo.svg";
 import avatar from "../../assets/avatar.png";
@@ -11,21 +13,31 @@ function Header({ handleAddClick, weatherData }) {
 
   return (
     <header className="header">
-      <img src={logo} alt="What to Wear" className="header__logo" />
+      <Link to="/">
+        <img src={logo} alt="What to Wear" className="header__logo" />
+      </Link>
       <p className="header__date-location">
         {currentDate}, {weatherData.city}
       </p>
-      <ToggleSwitch />
-      <button
-        type="button"
-        className="header__add-clothes-btn"
-        onClick={handleAddClick}
-      >
-        + Add clothes
-      </button>
-      <div className="header__user-container">
-        <p className="header__username">Terrence Tegegne</p>
-        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      <div className="header__mobile-menu">
+        <ToggleSwitch />
+        <button
+          type="button"
+          className="header__add-clothes-btn"
+          onClick={handleAddClick}
+        >
+          + Add clothes
+        </button>
+        <Link to="/profile">
+          <div className="header__user-container">
+            <p className="header__username">Terrence Tegegne</p>
+            <img
+              src={avatar}
+              alt="Terrence Tegegne"
+              className="header__avatar"
+            />
+          </div>
+        </Link>
       </div>
     </header>
   );
